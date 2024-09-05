ec62f1b5bec8f834e24dea646c00661c7e304498
Juan Goler <juanandreigoler@gmail.com>
2024-09-05 00:47:42 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrimo(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &num);
+
+    if (isPrimo(num))
+        printf("%d é um número primo.\n", num);
+    else
+        printf("%d não é um número primo.\n", num);
+
+    return 0;
+}
